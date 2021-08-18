f218e4e36e37ea9b5b7b2d4a0aa87f876275a57a
Marina <mc-p@ya,ru>
2021-08-18 11:49:03 +0300

    ...

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.gui_caht_0988 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.gui_caht_0988 to javafx.fxml;
+    exports com.example.gui_caht_0988;
+}
